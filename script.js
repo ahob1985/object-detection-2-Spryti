2f5a9045138556ea3d0f6db88f9feb86f325b62b
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-24 20:59:22 +0000

    Almost finished project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Author:
+// Author: Jordan Muturi
 
 // Global UI Variables
 let canvasDiv;
@@ -13,28 +13,55 @@ let detections;
 let isModelReady;
 
 function setup() {
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading please wait...");
+  textP.parent(textDiv);
+  video = createCapture(VIDEO, videoReady);
+  detections = [];
+  isModelReady = false;
 
 }
 
 function draw() {
-
+  if(isModelReady === true) {
+    image(video, 0, 0);
+    de
+  }
 }
 
-function videoReady() {
+function videoReady() { 
+  video.style("display", "none");
+  detector = ml5.objectDetector("cossed", modelReady);
 
 }
 
 function modelReady() {
-
+  isModelReady = true
 }
 
 function drawLabel(object) {
   // Draw a rectangular outline around the object
-
+  stroke(255, 255, 0);
+  noFill();
+  rect(object.x, object.y, object.width, object.height);
   // Draw the label and its confidence value near the object
+  noStroke
+  fill(0, 255, 0);
+  textSize(20);
+  let label = object.label;
+  let confidence = floor(object.confidence * 100);
+  text(label + ": " + confidence + "% ", object.x + 10, object.y + 20);
 
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  } else {
+    textP.html("I detect the things!");
+    detections = results;
+  }
 }
